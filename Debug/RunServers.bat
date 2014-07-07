73fd89a70eadb0b6cb7a0d5eacb89a29c2fdf806
Atidote <atidote63240@gmail.com>
2014-07-07 14:40:01 +0200

    ok
    refrshing my git

--- Debug/RunServers.bat
+++ Debug/RunServers.bat
@@ -2,5 +2,8 @@ echo Starting DBO Servers
 start AuthServer.exe
 echo AuthServer Started
 start CharServer.exe
+echo CharServer Started
 start ChatServer.exe
+echo ChatServer Started
 start GameServer.exe
+echo GameServer Started
