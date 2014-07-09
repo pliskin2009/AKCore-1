1b846f5b72f407eb09e5a48b3c1332c17046841f
Atidote <atidote63240@gmail.com>
2014-07-09 21:32:35 +0200

    basics2
    basics2

--- Server/GameServer/PlayerInfos.cpp
+++ Server/GameServer/PlayerInfos.cpp
@@ -389,110 +389,110 @@ void		PlayerInfos::SavePlayerData()
 
 	// 1 DONE
 	this->db->prepare(save_query2);
-	this->db->setInt(1, 0); //BaseLpRegen
-	this->db->setInt(2, 0); // LastLpRegen
-	this->db->setInt(3, 0);//this->BaseLpSitdownRegen);
-	this->db->setInt(4, 0);//LastLpSitdownRegen
-	this->db->setInt(5, 0);//BaseLpBattleRegen
-	this->db->setInt(6, 0);//LastLpBattlerRegen
-	this->db->setInt(7, 0);//this->BaseEpRegen);
-	this->db->setInt(8, 0);//this->LastEpRegen);
-	this->db->setInt(9, 0);//this->BaseRpSitdownRegen);
-	this->db->setInt(10, 0);//this->LastEpSitdownRegen
-	this->db->setInt(11, 0);//this->BaseEpBattleRegen
-	this->db->setInt(12, 0);//this->LastEpBattleRegen
-	this->db->setInt(13, 0);//this->BaseRpRegen
-	this->db->setInt(14, 0);//this->LastRpRegen
-	this->db->setInt(15, 0);//this->LastRpDimimutionRate
-	this->db->setInt(16, this->BasePhysicalOffence);//this->BasePhysicalOffence
-	this->db->setInt(17, this->LastPhysicalOffence);//this->LastPhysicalOffence
-	this->db->setInt(18, this->BasePhysicalDefence);//this->BasePhysicalDefence
+	this->db->setInt(1, this->BaseLpRegen);
+	this->db->setInt(2, this->LastLpRegen);
+	this->db->setInt(3, this->BaseLpSitdownRegen);
+	this->db->setInt(4, this->LastLpSitdownRegen);
+	this->db->setInt(5, this->BaseLpBattleRegen);
+	this->db->setInt(6, this->LastLpBattleRegen);
+	this->db->setInt(7, this->BaseEpRegen);
+	this->db->setInt(8, this->LastEpRegen);
+	this->db->setInt(9, this->BaseEpSitdownRegen);
+	this->db->setInt(10, this->LastEpSitdownRegen);
+	this->db->setInt(11, this->BaseEpBattleRegen);
+	this->db->setInt(12, this->LastEpBattleRegen);
+	this->db->setInt(13, this->BaseRpRegen);
+	this->db->setInt(14, this->LastRpRegen);
+	this->db->setInt(15, this->LastRpDimimutionRate);
+	this->db->setInt(16, this->BasePhysicalOffence);
+	this->db->setInt(17, this->LastPhysicalOffence);
+	this->db->setInt(18, this->BasePhysicalDefence);
 	this->db->setInt(19, this->charID);
 	this->db->execute();
 
 	// 2 DONE
 	this->db->prepare(save_query3);
-	this->db->setInt(1, this->LastPhysicalDefence);//this->LastPhysicalDefence
-	this->db->setInt(2, this->BaseEnergyOffence);//this->BaseEnergyOffence
-	this->db->setInt(3, this->LastEnergyOffence);//this->LastEnergyOffence
-	this->db->setInt(4, this->BaseEnergyDefence);//this->BaseEnergyDefence
-	this->db->setInt(5, this->LastEnergyDefence);//this->LastEnergyDefence
-	this->db->setInt(6, this->BaseAttackRate);//this->BaseAttackRate
-	this->db->setInt(7, this->LastAttackRate);//this->BaseAttackRate
-	this->db->setInt(8, this->BaseDodgeRate);//this->BaseDogeRate
-	this->db->setInt(9, this->LastDodgeRate);//this->LastDodgeRate
-	this->db->setInt(10, this->BaseBlockRate);//this->BaseBlockRate
-	this->db->setInt(11, this->BaseBlockRate);//this->BaseBlockRate
-	this->db->setInt(12, this->LastBlockRate);//this->LastBlockRate
-	this->db->setInt(13, 0);//this->BaseCurseSucessRate
-	this->db->setInt(14, 0);//this->LastCurseSucessRate
-	this->db->setInt(15, 0);//this->BaseCurseToleranceRate
-	this->db->setInt(16, 0);//this->LastCurseToleranceRate
-	this->db->setInt(17, 0);//this->BasePhysicalCriticalRate
-	this->db->setInt(18, 0);//this->LastPhysicalCriticalRate
+	this->db->setInt(1, this->LastPhysicalDefence);
+	this->db->setInt(2, this->BaseEnergyOffence);
+	this->db->setInt(3, this->LastEnergyOffence);
+	this->db->setInt(4, this->BaseEnergyDefence);
+	this->db->setInt(5, this->LastEnergyDefence);
+	this->db->setInt(6, this->BaseAttackRate);
+	this->db->setInt(7, this->LastAttackRate);
+	this->db->setInt(8, this->BaseDodgeRate);
+	this->db->setInt(9, this->LastDodgeRate);
+	this->db->setInt(10, this->BaseBlockRate);
+	this->db->setInt(11, this->BaseBlockRate);
+	this->db->setInt(12, this->LastBlockRate);
+	this->db->setInt(13, this->BaseCurseSuccessRate);
+	this->db->setInt(14, this->LastCurseSuccessRate);
+	this->db->setInt(15, this->BaseCurseToleranceRate);
+	this->db->setInt(16, this->LastCurseToleranceRate);
+	this->db->setInt(17, this->BasePhysicalCriticalRate);
+	this->db->setInt(18, this->LastPhysicalCriticalRate);
 	this->db->setInt(19, this->charID);
 	this->db->execute();
 
 	//3 DONE
 	this->db->prepare(save_query4);
-	this->db->setInt(1, 0);//this->BaseEnergyCriticalRate
-	this->db->setInt(2, this->LastEnergyCriticalRate);//this->LastEnergyCriticalRate
-	this->db->setInt(3, this->LastRunSpeed);//this->LastRunSpeed
-	this->db->setInt(4, 0);//this->BaseAttackSpeedRate
-	this->db->setInt(5, 0);//this->BaseAttackRange
-	this->db->setInt(6, 0);//this->LastAttackRange
-	this->db->setInt(7, 0);//this->CastingTimeChangePercent
-	this->db->setInt(8, 0);//this->CoolTimeChangePercent
-	this->db->setInt(9, 0);//this->KeepTimeChangePercent
-	this->db->setInt(10, 0);//this->DotValueChangePercent
-	this->db->setInt(11, 0);//this->DotTimeChangeAbsolute
-	this->db->setInt(12, 0);//this->RequiredEpChangePercent
-	this->db->setInt(13, 0);//this->HonestOffence
-	this->db->setInt(14, 0);//this->HonestDefence
-	this->db->setInt(15, 0);//this->StrangeOffence
-	this->db->setInt(16, 0);//this->StrangeDefence
-	this->db->setInt(17, 0);//this->WildOffence
-	this->db->setInt(18, 0); //this->WildDefence
+	this->db->setInt(1, this->BaseEnergyCriticalRate);
+	this->db->setInt(2, this->LastEnergyCriticalRate);
+	this->db->setInt(3, this->LastRunSpeed);
+	this->db->setInt(4, this->BaseAttackSpeedRate);
+	this->db->setInt(5, this->BaseAttackRange);
+	this->db->setInt(6, this->LastAttackRange);
+	this->db->setInt(7, this->CastingTimeChangePercent);
+	this->db->setInt(8, this->CoolTimeChangePercent);
+	this->db->setInt(9, this->KeepTimeChangePercent);
+	this->db->setInt(10, this->DotValueChangePercent);
+	this->db->setInt(11, this->DotTimeChangeAbsolute);
+	this->db->setInt(12, this->RequiredEpChangePercent);
+	this->db->setInt(13, this->HonestOffence);
+	this->db->setInt(14, this->HonestDefence);
+	this->db->setInt(15, this->StrangeOffence);
+	this->db->setInt(16, this->StrangeDefence);
+	this->db->setInt(17, this->WildOffence);
+	this->db->setInt(18, this->WildDefence);
 	this->db->setInt(19, this->charID);
 	this->db->execute();
 
 	//4 DONE
 	this->db->prepare(save_query5);
-	this->db->setInt(1, 0);//this->EleganceOffence
-	this->db->setInt(2, 0);//this->EleganceDefence
-	this->db->setInt(3, 0);//this->FunnyDefence
-	this->db->setInt(4, 0);//this->FunnyOffence
-	this->db->setInt(5, 0);//this->FunnyDefence
-	this->db->setInt(6, 0);//this->ParalyzeToleranceRate
-	this->db->setInt(7, 0);//this->TerrorToleranceRate
-	this->db->setInt(8, 0);//this->ConfuseToleranceRate
-	this->db->setInt(9, 0);//this->StoneToleranceRate
-	this->db->setInt(10, 0);//this->CandyToleranceRate
-	this->db->setInt(11, 0);//this->ParalyzeKeepTimeDown
-	this->db->setInt(12, 0);//this->TerrorKeepTimeDown
-	this->db->setInt(13, 0);//this->ConfuzeKeepTimeDown
-	this->db->setInt(14, 0);//this->StoneKeepTimeDown
-	this->db->setInt(15, 0);//this->CandyKeepTimeDown
-	this->db->setInt(16, 0);//this->BleedindKeepTimeDown
-	this->db->setInt(17, 0);//this->PoisonKeepTimeDown
-	this->db->setInt(18, 0);//this->StomachacheKeepTimeDown
-	this->db->setInt(19, 0);//this->CriticalBlockSuccessRate
+	this->db->setInt(1, this->EleganceOffence);
+	this->db->setInt(2, this->EleganceDefence);
+	this->db->setInt(3, this->FunnyDefence);
+	this->db->setInt(4, this->FunnyOffence);
+	this->db->setInt(5, this->FunnyDefence);
+	this->db->setInt(6, this->ParalyzeToleranceRate);
+	this->db->setInt(7, this->TerrorToleranceRate);
+	this->db->setInt(8, this->ConfuseToleranceRate);
+	this->db->setInt(9, this->StoneToleranceRate);
+	this->db->setInt(10, this->CandyToleranceRate);
+	this->db->setInt(11, this->ParalyzeKeepTimeDown);
+	this->db->setInt(12, this->TerrorKeepTimeDown);
+	this->db->setInt(13, this->ConfuseKeepTimeDown);
+	this->db->setInt(14, this->StoneKeepTimeDown);
+	this->db->setInt(15, this->CandyKeepTimeDown);
+	this->db->setInt(16, this->BleedingKeepTimeDown);
+	this->db->setInt(17, this->PoisonKeepTimeDown);
+	this->db->setInt(18, this->StomachacheKeepTimeDown);
+	this->db->setInt(19, this->CriticalBlockSuccessRate);
 	this->db->setInt(20, this->charID);
 	this->db->execute();
 
 	//5 DONE
 	this->db->prepare(save_query6);
-	this->db->setInt(1, 0); //this->GuardRate
-	this->db->setInt(2, 0);//this->SkillDamageBlockModeSuccessRate
-	this->db->setInt(3, 0);//this->CurseBlockModeSuccessRate
-	this->db->setInt(4, 0);//this->KnockdownBlockModeSuccessRate
-	this->db->setInt(5, 0);//this->HtbBlockModeSuccessRate
-	this->db->setInt(6, 0);//this->SitDownLpRegenBonusRate
-	this->db->setInt(7, 0);//this->SitDownEpRegenBonusRate
-	this->db->setInt(8, 0);//this->PhysicalCriticalDamageBonusRate
-	this->db->setInt(9, 0);//this->EnergyCriticalDamageBonusRate
-	this->db->setInt(10, 0);//this->ItemUpgradeBonusRate
-	this->db->setInt(11, 0);//this->ItemUpgradeBreakBonusRate
+	this->db->setInt(1, this->GuardRate);
+	this->db->setInt(2, this->SkillDamageBlockModeSuccessRate);
+	this->db->setInt(3, this->CurseBlockModeSuccessRate);
+	this->db->setInt(4, this->KnockdownBlockModeSuccessRate);
+	this->db->setInt(5, this->HtbBlockModeSuccessRate);
+	this->db->setInt(6, this->SitDownLpRegenBonusRate);
+	this->db->setInt(7, this->SitDownEpRegenBonusRate);
+	this->db->setInt(8, this->PhysicalCriticalDamageBonusRate);
+	this->db->setInt(9, this->EnergyCriticalDamageBonusRate);
+	this->db->setInt(10, this->ItemUpgradeBonusRate);
+	this->db->setInt(11, this->ItemUpgradeBreakBonusRate);
 	this->db->setInt(12, this->CurLp);
 	this->db->setInt(13, this->CurEp);
 	this->db->setInt(14, this->CurRp);
@@ -515,25 +515,34 @@ void		PlayerInfos::setZero()
 
 void		PlayerInfos::calculeMyStat(CGameServer * app)
 {
-	CNtlPacket packet4(sizeof(sGU_AVATAR_ATTRIBUTE_UPDATE));
-	sGU_AVATAR_ATTRIBUTE_UPDATE * res4 = (sGU_AVATAR_ATTRIBUTE_UPDATE *)packet4.GetPacketData();
-
 	app->db->prepare("SELECT * FROM items WHERE owner_ID = ? AND place=7 ORDER BY pos ASC");
 	app->db->setInt(1, this->GetCharID());
 	app->db->execute();
-	int loop = app->db->rowsCount();
+
 	CItemTable *itemTbl = app->g_pTableContainer->GetItemTable();
-	this->setZero(); //base stat
-	printf("Attribute list begin:\n");
+
+	this->LastPhysicalOffence = this->BasePhysicalOffence;
+	this->LastPhysicalDefence = this->BasePhysicalDefence;
+	this->LastEnergyOffence = this->BaseEnergyOffence;
+	this->LastEnergyDefence = this->BaseEnergyDefence;
+	this->LastAttackSpeedRate = this->BaseAttackSpeedRate;
+	this->LastAttackRange = 0;//this->BaseAttackRange;
+
 	while (app->db->fetch())
 	{
 		sITEM_TBLDAT* pItemData = (sITEM_TBLDAT*) itemTbl->FindData(app->db->getInt("tblidx"));
-		pItemData->wPhysical_Offence;
-		pItemData->wPhysical_Defence;
-		pItemData->wEnergy_Offence;
-		pItemData->wEnergy_Defence;
-		pItemData->wAttack_Speed_Rate;
-		pItemData->fAttack_Range_Bonus;
+		if (pItemData->wPhysical_Offence < 65535 && pItemData->wPhysical_Offence > 0)
+			this->LastPhysicalOffence += pItemData->wPhysical_Offence;
+		if (pItemData->wPhysical_Defence < 65535 && pItemData->wPhysical_Defence > 0)
+			this->LastPhysicalDefence += pItemData->wPhysical_Defence;
+		if (pItemData->wEnergy_Offence < 65535 && pItemData->wEnergy_Offence > 0)
+			this->LastEnergyOffence += pItemData->wEnergy_Offence;
+		if (pItemData->wEnergy_Defence < 65535 && pItemData->wEnergy_Defence > 0)
+			this->LastEnergyDefence += pItemData->wEnergy_Defence;
+		if (pItemData->wAttack_Speed_Rate < 65535 && pItemData->wAttack_Speed_Rate > 0)
+			this->LastAttackSpeedRate += pItemData->wAttack_Speed_Rate;
+		if (pItemData->fAttack_Range_Bonus < 65535 && pItemData->fAttack_Range_Bonus > 0)
+			this->LastAttackRange += pItemData->fAttack_Range_Bonus;
 		pItemData->dwPhysical_OffenceUpgrade;
 		pItemData->dwPhysical_DefenceUpgrade;
 		pItemData->dwEnergy_OffenceUpgrade;
@@ -545,9 +554,10 @@ void		PlayerInfos::calculeMyStat(CGameServer * app)
 		pItemData->byNeed_Con;
 		pItemData->byNeed_Dex;
 		pItemData->byBattle_Attribute;
-		printf("Item: %d have attribute:\n", app->db->getInt("tblidx"));
+		/*printf("Item: %d have attribute:\n", app->db->getInt("tblidx"));
 		printf("%d, %d, %d, %d, %d, %d %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n",pItemData->wPhysical_Offence ,pItemData->wPhysical_Defence ,pItemData->wEnergy_Offence ,pItemData->wEnergy_Defence ,pItemData->wAttack_Speed_Rate,
 			pItemData->fAttack_Range_Bonus,pItemData->dwPhysical_OffenceUpgrade ,pItemData->dwPhysical_DefenceUpgrade ,pItemData->dwEnergy_OffenceUpgrade ,pItemData->dwEnergy_DefenceUpgrade ,pItemData->byNeed_Str,
-			pItemData->byNeed_Sol,pItemData->byNeed_Foc ,pItemData->byNeed_Eng ,pItemData->byNeed_Con ,pItemData->byNeed_Dex,pItemData->byBattle_Attribute );
+			pItemData->byNeed_Sol,pItemData->byNeed_Foc ,pItemData->byNeed_Eng ,pItemData->byNeed_Con ,pItemData->byNeed_Dex,pItemData->byBattle_Attribute );*/
+		printf("%d, %d, %d, %d, %d, %d\n",this->LastPhysicalOffence ,this->LastPhysicalDefence ,this->LastEnergyOffence ,this->LastEnergyDefence ,this->LastAttackSpeedRate ,this->LastAttackRange );
 	}
 }
